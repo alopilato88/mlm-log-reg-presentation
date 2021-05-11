c27c13b1dbf4ba8e1842ae4e421a76c291b8c91e
Alex LoPilato <alex.lopilato@gmail.com>
2021-05-11 14:24:20 -0400

    Add packages and source functions.R.

---
+++ R/01-dependencies.R
@@ -0,0 +1,14 @@
+######################################################################
+# This script loads all of the packages and user-defined functions   #
+# used by all remaining scripts in this repo.                        #
+######################################################################
+
+# Load packages
+library(dplyr)
+library(tidyr)
+library(tibble)
+library(magrittr)
+library(lme4)
+
+# Source user-defined functions
+source("../R/functions.R")
