ac6802097a6551e8566e8de88e9a9a1b35bec3c2
Alex LoPilato <alex.lopilato@gmail.com>
2021-05-11 16:51:52 -0400

    Create simulated logistic regression data. Create simulated mutilevel team data.

---
+++ R/03-simulated-data.R
@@ -0,0 +1,40 @@
+#############################################################################
+# This script simulates data from a mixed-effects logistic regression model #
+#############################################################################
+
+# Create logistic regression example
+sim_logistic_reg_output <-
+  glmer_logistic_sim(
+    n_ind = 1000,
+    n_cluster = 2,
+    odds_ratio_x = c(exp(logit(.35)),
+                     10,
+                     .25),
+    odds_ratio_cluster_x = NA,
+    odds_ratio_inx = NA,
+    ranef_covariance = matrix(0, ncol = 1)
+  )
+
+sim_logistic_reg_data <-
+  sim_logistic_reg_output %>%
+  purrr::pluck(
+    "data"
+  )
+
+# Create simulated multilevel team data for extended example
+sim_team_output <-
+  glmer_logistic_sim(
+    n_ind = 10,
+    n_cluster = 150,
+    odds_ratio_x = c(exp(logit(.35)),
+                     2.50),
+    odds_ratio_cluster_x = c(2),
+    odds_ratio_inx = 2
+  )
+
+# Pluck data from sim_team_output -- think of examples
+sim_team_data <-
+  sim_team_output %>%
+  purrr::pluck(
+    "data"
+  )
