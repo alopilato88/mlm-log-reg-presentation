be47e047750456686cb31430b9f7b7229d114aea
alopilato88 <alex.lopilato@gmail.com>
2021-05-13 09:09:03 -0400

    Fit models for GLMER sections.

--- R/04-model-fitting.R
+++ R/04-model-fitting.R
@@ -20,5 +20,37 @@ logistic_m1 <- glm(Y ~ X_IND_1 + X_IND_2,
                    family = "binomial")
 
 # --- Fit empty GLMER model from GLMER introduction --- #
+glmer_m0 <- lme4::glmer(Y ~ 1 + (1 | CLUSTER_ID), 
+                        data = sim_empty_mod_data, 
+                        family = "binomial")
 
-# --- Fit cross-classified GLMER model for Hiring Recomendation example --- #
+# --- Fit cross-classified GLMER model for Hiring Recommendation example --- #
+glmer_hire_m0 <- lme4::glmer(HIRE_REC ~ 1 + 
+                               (1 | INTERVIEWER_ID) + (1 | CANDIDATE_ID), 
+                             data = hiring_data$data_sparse, 
+                             family = "binomial")
+
+glmer_hire_m1 <- lme4::glmer(HIRE_REC ~ CANDIDATE_GENDER + INTERVIEWER_GENDER + 
+                               (1 | INTERVIEWER_ID) + (1 | CANDIDATE_ID), 
+                             data = hiring_data$data_sparse, 
+                             family = "binomial")
+
+glmer_hire_m2 <- lme4::glmer(HIRE_REC ~ CANDIDATE_GENDER * INTERVIEWER_GENDER + 
+                               (1 | INTERVIEWER_ID) + (1 | CANDIDATE_ID), 
+                             data = hiring_data$data_sparse, 
+                             family = "binomial")
+
+glmer_hire_m3 <- lme4::glmer(HIRE_REC ~ CANDIDATE_GENDER * INTERVIEWER_GENDER + 
+                               ASSESSMENT_1_CAT + ASSESSMENT_2_CAT + 
+                               (1 | CANDIDATE_ID) + (1 | INTERVIEWER_ID), 
+                             data = hiring_data$data_sparse, 
+                             family = "binomial")
+
+glmer_hire_m4 <- lme4::lmer(ASSESSMENT_1_CAT ~ CANDIDATE_GENDER * INTERVIEWER_GENDER + 
+                              (1 | INTERVIEWER_GENDER) + (1 | CANDIDATE_GENDER), 
+                            data = hiring_data$data_sparse)
+
+glmer_hire_m5 <- lme4::lmer(ASSESSMENT_2_CAT ~ CANDIDATE_GENDER * INTERVIEWER_GENDER + 
+                              (1 | INTERVIEWER_GENDER) + (1 | CANDIDATE_GENDER), 
+                            data = hiring_data$data_sparse)
+
