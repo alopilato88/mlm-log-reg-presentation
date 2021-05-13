5c911b5f4c98493ce3ae19dad4d040f5fe0e0b95
alopilato88 <alex.lopilato@gmail.com>
2021-05-13 08:49:29 -0400

    Fit models for logistic regression primer section.

--- R/04-model-fitting.R
+++ R/04-model-fitting.R
@@ -11,6 +11,13 @@ lmer_m2 <- lme4::lmer(Y ~ X_CLUSTER_CENT + X_CLUSTER_MEAN + (1 | CLUSTER_ID),
                       data = lmer_data)
 
 # --- Fit logistic model for logistic primer section --- #
+logistic_m0 <- glm(Y ~ 1, 
+                   data = sim_logistic_reg_data,
+                   family = "binomial")
+
+logistic_m1 <- glm(Y ~ X_IND_1 + X_IND_2, 
+                   data = sim_logistic_reg_data,
+                   family = "binomial")
 
 # --- Fit empty GLMER model from GLMER introduction --- #
 
