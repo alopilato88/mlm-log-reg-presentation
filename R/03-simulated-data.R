319143da84234829bf89d908adf4c5fa071f9ae4
Alex LoPilato <alex.lopilato@gmail.com>
2021-07-09 08:59:26 -0400

    Update sim_logistic_reg_data to simulate X variables.

--- R/03-simulated-data.R
+++ R/03-simulated-data.R
@@ -28,17 +28,29 @@ sim_logistic_reg_data <-
 sim_empty_mod_output <- 
   glmer_logistic_sim(
     n_ind = 100,
-    n_cluster = 150,
-    odds_ratio_x = exp(logit(.50)),
-    odds_ratio_cluster_x = NA,
+    n_cluster = 200,
+    odds_ratio_x = c(exp(0), exp(1)),
+    odds_ratio_cluster_x = exp(4.20),
     odds_ratio_inx = NA,
-    ranef_covariance = matrix(1, ncol = 1)
+    ranef_covariance = matrix(3, ncol = 1)
   )
 
 sim_empty_mod_data <- 
   sim_empty_mod_output %>%
   purrr::pluck(
     "data"
+  ) %>%
+  dplyr::select(
+    JOB_SAT_BINARY = Y,
+    SCALED_TENURE = X_IND_1,
+    HR_PRACTICES = X_CLUSTER_1,
+    FIRM = CLUSTER_ID
+  ) %>%
+  dplyr::mutate(
+    JOB_SAT_TEXT = dplyr::case_when(
+      JOB_SAT_BINARY == 1 ~ "Satisfied",
+      TRUE ~ "Not Satisfied"
+    )
   )
 
 # --- Create cross-classified hiring recommendation simulated data -- #
